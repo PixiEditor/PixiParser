64d57d8fe079b5a39391b2b176b792da50ac77b8
CPKreuz <chrisphilkreuzgaming04@gmail.com>
2021-04-13 23:54:31 +0200

    Added benchmark project

---
+++ src/PixiParser.Benchmarks/Benchmarks/DeserializationBenchmarks.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace PixiEditor.Parser.Benchmarks
+{
+    [SimpleJob(RuntimeMoniker.NetCoreApp50, baseline: true)]
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    [HtmlExporter]
+    [MarkdownExporterAttribute.GitHub]
+    public class DeserializationBenchmarks
+    {
+        private byte[] benchmarkDocument;
+
+        [Params(32, 64, 1920)]
+        public int Size;
+
+        [Params(1, 4)]
+        public int Layers;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            benchmarkDocument = PixiParser.Serialize(Helper.CreateDocument(Size, Layers));
+        }
+
+        [Benchmark]
+        public void Deserialize()
+        {
+            PixiParser.Deserialize(benchmarkDocument);
+        }
+    }
+}
---
+++ src/PixiParser.Benchmarks/Helper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PixiEditor.Parser.Benchmarks
+{
+    public static class Helper
+    {
+        public static SerializableDocument CreateDocument(int Size, int Layers)
+        {
+            var benchmarkDocument = new SerializableDocument()
+            {
+                Width = Size,
+                Height = Size,
+                Swatches = new Tuple<byte, byte, byte, byte>[] { new Tuple<byte, byte, byte, byte>(0, 0, 0, 0) },
+                Layers = new SerializableLayer[Layers]
+            };
+
+            for (int i = 0; i < Layers; i++)
+            {
+                var layer = benchmarkDocument.Layers[i] = new SerializableLayer();
+                layer.BitmapBytes = new byte[Size * Size * 4];
+
+                new Random().NextBytes(layer.BitmapBytes);
+            }
+
+            return benchmarkDocument;
+        }
+    }
+}
