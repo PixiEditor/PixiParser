5c7c10ee24db0dd08d470d1d6a8cc96c5089300f
Krzysztof Krysiński <flubbet@gmail.com>
2023-03-01 23:03:35 +0100

    Added reference layer corners

---
+++ src/PixiParser/Models/Corners.cs
@@ -0,0 +1,16 @@
+﻿using MessagePack;
+
+namespace PixiEditor.Parser;
+
+[MessagePackObject]
+public class Corners
+{
+    [Key(0)]
+    public Vector2 TopLeft { get; set; }
+    [Key(1)]
+    public Vector2 TopRight { get; set; }
+    [Key(2)]
+    public Vector2 BottomLeft { get; set; }
+    [Key(3)]
+    public Vector2 BottomRight { get; set; }
+}
---
+++ src/PixiParser/Models/Vector2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MessagePack;
+
+namespace PixiEditor.Parser;
+
+[MessagePackObject]
+public struct Vector2 : IEquatable<Vector2>
+{
+    [Key(0)]
+    public double X { set; get; }
+    
+    [Key(1)]
+    public double Y { set; get; }
+
+    public bool Equals(Vector2 other) => X.Equals(other.X) && Y.Equals(other.Y);
+
+    public override bool Equals(object obj) => obj is Vector2 other && Equals(other);
+
+    public override int GetHashCode() => HashCode.Combine(X, Y);
+}
