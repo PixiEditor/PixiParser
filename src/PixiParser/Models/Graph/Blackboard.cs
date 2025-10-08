63556d138fc93c3e4ad2441cb9b840923df396a4
Krzysztof Krysiński <flubbet@gmail.com>
2025-10-08 13:03:44 +0200

    Added blackboard

---
+++ src/PixiParser/Models/Graph/Blackboard.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MessagePack;
+
+namespace PixiEditor.Parser.Graph;
+
+[MessagePackObject]
+public class Blackboard
+{
+    [Key(0)]
+    public List<Variable> Variables { get; set; } = new();
+}
